9643311fd892ba8edc937c01e88c4c19efff848e
girlswhocodee <myekini1@umbc.edu>
2020-08-01 19:30:00 -0400

    Basic Operations Complete

---
+++ answers/exercise12.sql
@@ -0,0 +1,2 @@
+ALTER TABLE Users
+    RENAME COLUMN MiddleName TO Initial; 
---
+++ answers/exercise5.sql
@@ -0,0 +1,7 @@
+CREATE TABLE Users (
+    UserID INT,
+    Lastname VARCHAR(255),
+    FirstName VARCHAR(255),
+    Address VARCHAR(255),
+    City VARCHAR(255), 
+);
